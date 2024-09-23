945552caf95451e82908fa5abf6bffda959ef320
Draweshwarkumar <shivamjha22948@gmail.com>
2024-09-23 14:23:34 +0530

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatButtonModule} from '@angular/material/button';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync('noop')]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync('noop'), provideAnimationsAsync(),MatButtonModule]
 };
